271c2f0aaabfdcff836a25fdb1f18b962fe90442
Davi Kawasaki <davishinjik@gmail.com>
2017-08-23 16:14:00 -0300

    Updating directory root for express

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname));
 app.use('/',function(req, res) {
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 app.listen(port);
 

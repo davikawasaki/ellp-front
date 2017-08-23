d49c27ffe30b566765e015afba7a3975538a5dc4
Davi Kawasaki <davishinjik@gmail.com>
2017-08-23 17:53:51 -0300

    About page model

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
-app.use('/',function(req, res) {
+
+app.get('/',function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.route('/about')
+  .get(function(req, res) {
+    console.log('Loaded about page');
+    res.sendFile(__dirname + '/about.html');  
+  });
+
+app.get('*', function(req, res){
+  res.redirect('/');
+});
+
 app.listen(port, function() {
   console.log("App is running on port: " + port);
 });

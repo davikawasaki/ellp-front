32f2db65e9322642ba3e5b3e0d90cb054cb711d7
Davi Kawasaki <davishinjik@gmail.com>
2017-08-23 16:25:02 -0300

    Fixing script to build application in heroku

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ app.use(express.static(__dirname));
 app.use('/',function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-app.listen(port);
+app.listen(port, function() {
+  console.log("App is running on port: " + port);
+});
 

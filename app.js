c977f89f0881db72b268a1a137445506cf07958e
Davi Kawasaki <davishinjik@gmail.com>
2017-08-24 16:59:01 -0300

    High school and children's day pages

--- app.js
+++ app.js
@@ -9,10 +9,24 @@ app.get('/',function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.route('/about')
+// app.route('/ongs')
+//   .get(function(req, res) {
+//     res.sendFile(__dirname + '/ongs.html');  
+//   });
+
+app.route('/diadascriancas')
   .get(function(req, res) {
-    console.log('Loaded about page');
-    res.sendFile(__dirname + '/about.html');  
+    res.sendFile(__dirname + '/diadascriancas.html');  
+  });
+
+// app.route('/engenhariaparamulheres')
+//   .get(function(req, res) {
+//     res.sendFile(__dirname + '/engenhariaparamulheres.html');  
+//   });
+
+app.route('/ensinomedio')
+  .get(function(req, res) {
+    res.sendFile(__dirname + '/ensinomedio.html');  
   });
 
 app.get('*', function(req, res){
